b6aa5a22d1c0227e6c1fc39a8b1ebbe7d5455772
rajca21 <2nikolar1@gmail.com>
2024-10-16 13:08:37 +0200

    updates for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/api', (req, res) => {
-  res.send('Backend is running');
-});
-
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
@@ -35,6 +31,17 @@ app.use('/api/upload', uploadRouter);
 // Setting dirname to current directory & setting the folder for uploads
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/api', (req, res) => {
+    res.send('Backend is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
